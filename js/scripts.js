216de3bd06fb6557d2b5e9f0f374c312308ca8dc
jakub-havlik <havlik9@gmail.com>
2022-01-03 15:29:20 +0100

    created an array of objects named "pokemonList"

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,39 @@
-alert('Hello world');
-
-let favoriteFood = 'Masala Dosa';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+    {
+        name: 'Venusaur',
+        type: ['grass', 'poison'],
+        height: 2,
+        pokedexNumber: '#3'
+    },
+    {
+        name: 'Gengar',
+        type: ['ghost', 'poison'],
+        height: 1.5,
+        pokedexNumber: '#94'
+    },
+    {
+        name: 'Blastoise',
+        type: 'water',
+        height: 1.6,
+        pokedexNumber: '#9'
+    },
+    {
+        name: 'Kakuna',
+        type: ['bug', 'poison'],
+        height: 0.6,
+        pokedexNumber: '#14'
+    },
+    {
+        name: 'Golbat',
+        type: ['poison', 'flying'],
+        height: 1.6,
+        pokedexNumber: '#42'
+    },
+    {
+        name: 'Togepi',
+        type: 'fairy',
+        height: 0.3,
+        pokedexNumber: '#175'
+    }
+];
