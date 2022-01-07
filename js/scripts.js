013309111c8080871b31e3a40d992d00fb542eb7
jakub-havlik <havlik9@gmail.com>
2022-01-07 10:08:17 +0100

    wrapped pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,104 @@
-let pokemonList = [
-    {
-        name: 'Venusaur',
-        types: ['grass', 'poison'],
-        height: 2,
-        pokedexNumber: '#3'
-    },
-    {
-        name: 'Gengar',
-        types: ['ghost', 'poison'],
-        height: 1.5,
-        pokedexNumber: '#94'
-    },
-    {
-        name: 'Blastoise',
-        types: 'water',
-        height: 1.6,
-        pokedexNumber: '#9'
-    },
-    {
-        name: 'Kakuna',
-        types: ['bug', 'poison'],
-        height: 0.6,
-        pokedexNumber: '#14'
-    },
-    {
-        name: 'Golbat',
-        types: ['poison', 'flying'],
-        height: 1.6,
-        pokedexNumber: '#42'
-    },
-    {
-        name: 'Togepi',
-        types: 'fairy',
-        height: 0.3,
-        pokedexNumber: '#175'
-    }
-];
+
+//wrap pokemonList array in an IIFE to avoid accidentally accessing the global state
+
+//new pokemonRepository variable to hold what the IIFE will return
+//assign the IIFE to that variable
+
+let pokemonRepository = (function () {
+
+  let pokemonList = [
+      {
+          name: 'Venusaur',
+          types: ['grass', 'poison'],
+          height: 2,
+          pokedexNumber: '#3'
+      },
+      {
+          name: 'Gengar',
+          types: ['ghost', 'poison'],
+          height: 1.5,
+          pokedexNumber: '#94'
+      },
+      {
+          name: 'Blastoise',
+          types: 'water',
+          height: 1.6,
+          pokedexNumber: '#9'
+      },
+      {
+          name: 'Kakuna',
+          types: ['bug', 'poison'],
+          height: 0.6,
+          pokedexNumber: '#14'
+      },
+      {
+          name: 'Golbat',
+          types: ['poison', 'flying'],
+          height: 1.6,
+          pokedexNumber: '#42'
+      },
+      {
+          name: 'Togepi',
+          types: 'fairy',
+          height: 0.3,
+          pokedexNumber: '#175'
+      }
+  ]
 
 
-//loop over the list of pokemons and write down their names and heights
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<br>' + pokemonList[i].name + ('  (height: ') + pokemonList[i].height + ')');
-    //comment the pokemons according to their heigth
-    //height between 0.5 and 1.7
-    if (pokemonList[i].height < 1.7 && pokemonList[i].height > 0.5) {
-      document.write(' - this pokemon is kind of average...');
-    }
-    //smaller or equal to 0.5
-    else if (pokemonList[i].height <= 0.5) {
-      document.write(' - this pokemon is really tiny!');
-    }
-    //larger or equal to 1.7
-    else {
-      document.write(' - this pokemon is bigger than I thought!');
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'types' in pokemon &&
+      'height' in pokemon &&
+      'pokedexNumber' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Pokemon is not correct')
     }
   }
+
+  //IIFE returns an object with public functions assigned as keys
+  return {
+    getAll: getAll,
+    add: add
+  }
+
+}) ()
+
+//console.log(pokemonRepository.getAll);
+//console.log(pokemonRepository.add);
+
+
+//add a new pokemon to the pokemonList
+let newPokemon = {
+    name: 'Weedle',
+    types: ['bug', 'poison'],
+    height: 1,
+    pokedexNumber: '#13'
+};
+
+pokemonRepository.add(newPokemon);
+
+
+//define function writing the list of pokemons
+function pokemonWrite(pokemon) {
+  let _html = `<br>${pokemon.name} (height: ${pokemon.height}) ${
+    pokemon.height < 1.7 && pokemon.height > 0.5
+      ? "- this pokemon is kind of average..."
+      : pokemon.height <= 0.5
+      ? "- this pokemon is really tiny!"
+      : "- this pokemon is bigger than I thought!"
+  }`;
+  document.write(_html);
+}
+//retrieve pokemonList from the IIFE
+let retrievePokemonList = pokemonRepository.getAll();
+//iterate over each pokemon in the repository using forEach() loop
+retrievePokemonList.forEach(pokemonWrite);
